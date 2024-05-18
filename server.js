151ce0e182682658be26bad5e5d7d0b9c2c65787
dominikabryla <116268325+dominikabryla@users.noreply.github.com>
2024-05-18 11:35:45 +0200

    Add

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 8000;
+
+const uri =
+  "mongodb+srv://dominikabrylaa:qxiyxSyYCNPdFaAU@myapppwa.11a24n3.mongodb.net/?retryWrites=true&w=majority&appName=myAppPWA";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  useUnifiedTopology: true,
+  tlsAllowInvalidCertificates: true,
+  tlsAllowInvalidHostnames: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const db = client.db("myapppwa");
+    const usersCollection = db.collection("users");
+
+    app.use(express.json());
+
+    app.post("/api/users", async (req, res) => {
+      const { username, password } = req.body;
+
+      try {
+        const existingUser = await usersCollection.findOne({ username });
+        if (existingUser) {
+          return res.status(400).json({ message: "Użytkownik już istnieje." });
+        }
+
+        const newUser = { username, password };
+        await usersCollection.insertOne(newUser);
+        res.status(201).json({ message: "Utworzono nowego użytkownika." });
+      } catch (error) {
+        console.error("Błąd podczas tworzenia użytkownika:", error);
+        res
+          .status(500)
+          .json({ message: "Wystąpił błąd podczas tworzenia użytkownika." });
+      }
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Serwer nasłuchuje na porcie ${PORT}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+run().catch(console.dir);
